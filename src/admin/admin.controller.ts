7ff3eba87ee235102b72ea8cc8a543b93394a01d
Mushfiqur Rahman Fahim <fahimrahman408@gmail.com>
2025-10-22 19:54:00 +0600

    guards added.

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { createUserDto } from 'src/dto files/cretaeUser.dto';
 import { CreateRoleDto } from 'src/dto files/Role.dto';
 import { LoginDto } from 'src/dto files/Login.dto';
+import { AdminGuard } from 'src/auth/adminGuard';
 
 @Controller('admin')
 export class AdminController {
@@ -14,11 +15,13 @@ export class AdminController {
     }
 
     @Post("createrole")
+    @UseGuards(AdminGuard)
      createrole(@Body() roledata:CreateRoleDto){
       return this.adminService.createrole(roledata)
      }
 
     @Post("createUser")
+    @UseGuards(AdminGuard)
     createUser(@Body() userData:createUserDto){
       return this.adminService.createUser(userData);
     }
